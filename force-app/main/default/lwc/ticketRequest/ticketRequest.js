be547d4e765511fe64230007ad8299ba32523e27
Cardoso <danilo.cardoso@accenture.com>
2019-09-19 15:53:00 +0100

    MVP done

--- force-app/main/default/lwc/ticketRequest/ticketRequest.js
+++ force-app/main/default/lwc/ticketRequest/ticketRequest.js
@@ -1,7 +1,7 @@
 import { LightningElement, track, wire } from 'lwc';
-import { refreshApex } from '@salesforce/apex';
 import { subscribe, unsubscribe, onError, setDebugFlag, isEmpEnabled } from 'lightning/empApi';
 import getSupportedLangs from '@salesforce/apex/TicketServerController.getSupportedLangs';
+import getTotalTrans from '@salesforce/apex/TicketServerController.getTotalTrans';
 import getTranslations from '@salesforce/apex/TicketServerController.getTranslations';
 import requestTranslation from '@salesforce/apex/TicketServerController.requestTranslation';
 
@@ -13,6 +13,8 @@ const columns = [
     { label: 'Status', fieldName: 'Status__c' }
 ];
 
+const numTranslations = 7; // number of records to be returned for the table
+
 export default class App extends LightningElement {
     @track loading = true;
     @track targetLang;
@@ -20,22 +22,57 @@ export default class App extends LightningElement {
     @track error;
     @track translations;
     @track originalText;
+    @track isBtnNewerDisabled = true;
+    @track isBtnOlderDisabled;
+    firstRecordDateTime;
+    lastRecordDateTime;
+    firstRecPos = 0;
+    LastRecPos = numTranslations - 1;
     subscription = {};
     wiredTranslationsResult; //so it can be refreshed programmatically
 
     @wire(getSupportedLangs)
     supportedLangs;
 
+    @wire(getTotalTrans)
+    totalTrans;
+
     get options() {
         return this.supportedLangs.data;
     }
 
-    refreshTable = () => {
+    refreshTable = (createdDateFilter, olderDataFlag) => {    
+        if (createdDateFilter) {
+            if (olderDataFlag === false) {
+                // button to get newer translations clicked
+                if (this.firstRecPos === 0) {
+                    return;
+                }
+            } else {
+                // button to get older translations clicked
+                if (this.LastRecPos === this.totalTrans.data-1) {
+                    return;
+                }
+            }
+        }
         this.loading = true;
-        getTranslations()
+        getTranslations({numRecords: numTranslations, dateFilter: createdDateFilter, older: olderDataFlag})
             .then(result => {
-                this.translations = result;
+                this.firstRecordDateTime = result[0].CreatedDate;
+                this.lastRecordDateTime = result[result.length-1].CreatedDate;
                 this.error = undefined;
+                if (createdDateFilter) {
+                    if (olderDataFlag === false) {
+                        // button to get newer translations clicked
+                        this.firstRecPos -= this.translations.length;
+                        this.LastRecPos -= this.translations.length;
+                    } else {
+                        // button to get older translations clicked
+                        this.firstRecPos += result.length;
+                        this.LastRecPos += result.length;
+                    }
+                }
+                this.translations = result;
             })
             .catch(error => {
                 this.error = error;
@@ -49,21 +86,20 @@ export default class App extends LightningElement {
     connectedCallback() {
         // initialize component
         // Callback invoked whenever a new event message is received
-        const messageCallback = function (response) {
-            console.log('New message received : ', JSON.stringify(response));
-            // Response contains the payload of the new message received
-            this.refreshTable();
-        };
+        const messageCallback = (function (response) {
+            console.log('New message received: ', JSON.stringify(response));
+            this.refreshTable(null);
+        }).bind(this);
 
         // Invoke subscribe method of empApi. Pass reference to messageCallback
-        subscribe('/topic/TranslationRequestUpdates', -1, messageCallback).then(response => {
+        subscribe('/topic/TranslationUpdates', -1, messageCallback).then(response => {
             // Response contains the subscription information on successful subscribe call
             console.log('Successfully subscribed to : ', JSON.stringify(response.channel));
             this.subscription = response;
         });
 
         // initializing table
-        this.refreshTable();
+        this.refreshTable(null);
     }
 
     handleTextChange(event) {
@@ -85,7 +121,7 @@ export default class App extends LightningElement {
             requestTranslation({ originalText: this.originalText, toLang: this.targetLang })
                 .then(() => {
                     this.error = undefined;
-                    this.refreshTable();
+                    this.refreshTable(this.lastRecordDateTime);
                 })
                 .catch(error => {
                     console.error(error);
@@ -98,6 +134,14 @@ export default class App extends LightningElement {
             let picklistCmp = this.template.querySelector('lightning-combobox');
             picklistCmp.reportValidity();
         }
+    }
+
+    handleNewerTrans() {
+        this.refreshTable(this.firstRecordDateTime, false);
+    }
+
+    handleOlderTrans() {
+        this.refreshTable(this.lastRecordDateTime, true);
     }
 
     disconnectedCallback() {
